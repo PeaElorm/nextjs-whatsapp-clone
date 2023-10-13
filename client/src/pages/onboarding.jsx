276a64468a7dc94e21947d18e347d112e4328d51
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-10-13 00:17:07 +0000

    create profile input field

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import { useStateProvider } from "@/context/StateContext";
+import Input from "@/components/common/Input";
+import { useState } from "react";
 
 function onboarding() {
   const [{ userInfo }] = useStateProvider();
+  const [name, setName] = useState(userInfo?.name || "");
+  const [about, setAbout] = useState("");
+  const [image, setImage] = useState("/default_avatar.png");
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
       <div className="flex items-center justify-center gap-2">
@@ -18,8 +23,9 @@ function onboarding() {
       </div>
       <h2 className="text-2xl">Create your profile</h2>
       <div className="flex gap-6 mt-6">
-        <div className="flex flec-col items-center justify-center mt-5 gap-6">
-          {userInfo.name}
+        <div className="flex flex-col items-center justify-center mt-5 gap-6">
+          <Input name="Display Name" state={name} setState={setName} label />
+          <Input name="About" state={about} setAbout={setAbout} label />
         </div>
       </div>
     </div>
