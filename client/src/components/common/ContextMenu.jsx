f8666359ddbb99969ae18a171313d358fda2dae3
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-10-17 20:55:40 +0000

    context menu starter

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, cordinates, ContextMenu, setContextMenu }) {
+  const contextMenuRef = useRef(null);
+  const handleClick = (e, callback) => {};
+  return (
+    <div
+      className={`bg-background-dropdown fixed py-2 z-[100] top-[${cordinates.y}] left-[${cordinates.x}] shadow-xl`}
+      ref={contextMenuRef}
+    >
+      <ul>
+        {options.map(({ name, callback }) => (
+          <li key={name} onClick={(e) => handleClick(e, callback)}>
+            <span className="text-white">{name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ContextMenu;
