564fa4f1df385c5599f307c87206ae96acf15f96
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-10-23 18:38:56 +0000

    onboarding auth  and login auth starter

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { useStateProvider } from "@/context/StateContext";
 import Input from "@/components/common/Input";
 import { useState } from "react";
 import Avatar from "@/components/common/Avatar";
+import { ONBOARD_USER_ROUTE } from "@/utils/ApiRoutes";
+import { useRouter } from "next/router";
 
 function onboarding() {
-  const [{ userInfo }] = useStateProvider();
+  const router = useRouter();
+  const [{ userInfo, newUser }] = useStateProvider();
   const [name, setName] = useState(userInfo?.name || "");
   const [about, setAbout] = useState("");
   const [image, setImage] = useState("/default_avatar.png");
+
+  useEffect(() => {
+    if (!newUser && !userInfo?.email) router.push("/login");
+    else if (!newUser && userInfo?.email) router.push("/");
+  }, [newUser, userInfo, router]);
+
+  const onboardUserHandler = async () => {
+    if (validateDetails()) {
+      const email = userInfo.email;
+      try {
+        const { data } = await axios.post(ONBOARD_USER_ROUTE, {
+          email,
+          name,
+          about,
+          image,
+        });
+        if (data.status) {
+          dispatch({ type: reducerCases.SET_NEW_USER, newUser: false });
+          dispatch({
+            type: reducerCases.SET_USER_INFO,
+            userInfo: {
+              name,
+              email,
+              profileImage: image,
+              status: about,
+            },
+          });
+          router.push("/");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const validateDetails = () => {
+    if (name.length < 3) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
       <div className="flex items-center justify-center gap-2">
@@ -27,6 +72,14 @@ function onboarding() {
         <div className="flex flex-col items-center justify-center mt-5 gap-6">
           <Input name="Display Name" state={name} setState={setName} label />
           <Input name="About" state={about} setAbout={setAbout} label />
+          <div className="flex items-center justify-center">
+            <button
+              className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
+              onClick={onboardUserHandler}
+            >
+              Create Profile
+            </button>
+          </div>
         </div>
         <div>
           <Avatar type="xl" image={image} setImage={setImage} />
